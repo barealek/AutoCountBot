ae9edb4f3cee996b5e08bab4812ea6c48ea63a08
Alek <92643775+barealek@users.noreply.github.com>
2023-09-08 14:55:15 +0200

    create go module

---
+++ src/devenv/devenvironments.go
@@ -0,0 +1,11 @@
+package devenv
+
+import (
+	"os"
+)
+
+func init() {
+	os.Setenv("COOLDOWN_TIME", "1")
+	os.Setenv("LISTENER_ID", "123456789012345678")
+	os.Setenv("TOKEN", "MTE0OTY3OTc5MDU4OTIxODgyNw.GcYjsU.uTCZpmQULKqZxBRfVd6IPSM9qx2YBjpmNv-PQ4")
+}
